e9dd4e0af6a4943041156a64c4bb19f01988da8a
sara <smhumphries@hotmail.co.uk>
2022-01-05 15:36:09 +0000

    gets api data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { useState, useEffect } from 'react';
 
 import LandingPage from "./components/Landing";
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+
+  const [weatherData, setWeatherData] = useState([]);
+
+  useEffect(() => {
+    fetchApiData('2022-01-01');
+  }, []);
+
+  const fetchApiData = (date) => {
+    fetch(`https://api.weatherapi.com/v1/history.json?key=dbdf40f685a9446e9c3144301220501&q&q=Edinburgh&dt=${date}`)
+      .then(result => result.json())
+      .then(res => setWeatherData(res));
+  };
+
   return (
     <div>
       <Header></Header>
       <Router>
       
           <Routes>
-            <Route path='/' element={ <LandingPage></LandingPage> }></Route>
+            <Route path='/' element={ <LandingPage weatherData={weatherData}></LandingPage> }></Route>
           </Routes>
       
       </Router>
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,10 @@
 import {} from 'react-bootstrap';
 
-const LandingPage = () => {
+const LandingPage = ({weatherData}) => {
     return (
         <div className='container'>
-            <h2>Landing</h2>
+            <p>The temperature in {weatherData.location.name} on {weatherData.forecast.forecastday[0].date} at {weatherData.forecast.forecastday[0].hour[12].time} was {weatherData.forecast.forecastday[0].hour[12].temp_c} degrees C </p>
+            {/* <p>The temperature at {weatherData.forecast.forecastday.hour[12].time} was</p> */}
         </div>
     )
 };
